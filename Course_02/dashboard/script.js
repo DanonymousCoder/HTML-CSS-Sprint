8477acc08a53e925a8174920817260248ebec76c
Ridwanullah <aladebaruwa@gmail.com>
2025-06-15 21:43:28 +0100

    refactoring...

--- Course_02/dashboard/script.js
+++ Course_02/dashboard/script.js
@@ -1,14 +1,12 @@
-let ham = document.getElementById("ham");
 let asideNav = document.getElementById("aside-nav");
 let closeNav = document.getElementById("close-nav");
 
-ham.addEventListener("click", openNav());
 closeNav.addEventListener("click", toCloseNav());
 
 function openNav() {
-    asideNav.classList.add("active");
+    asideNav.style.transform = "translateX(0)";
 }
 
 function toCloseNav() {
-    asideNav.classList.remove("active");
+    asideNav.style.transform = "translateX(-120%)";
 }
