9659fc57caa4145227fb7786cd65a0c8d6cf0e98
Ridwanullah <aladebaruwa@gmail.com>
2025-06-16 09:40:31 +0100

    fixed a bug 🛠️, now refactoring...

--- Course_02/dashboard/script.js
+++ Course_02/dashboard/script.js
@@ -2,13 +2,14 @@ let asideNav = document.getElementById("aside-nav");
 let closeNav = document.getElementById("close-nav");
 let ham = document.getElementById("ham");
 
-closeNav.addEventListener("click", toCloseNav());
-ham.addEventListener("click", openNav());
+closeNav.addEventListener("click", toCloseNav);
+ham.addEventListener("click", openNav);
+
 
 function openNav() {
     asideNav.style.transform = "translateX(0)";
 }
 
 function toCloseNav() {
-    asideNav.style.transform = "translateX(-200%)";
+    asideNav.style.transform = "translateX(-100%)";
 }
