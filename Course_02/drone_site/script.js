1dbddad43a23b5bf12ca9ec1b090c2afda7a59d4
Ridwanullah <aladebaruwa@gmail.com>
2025-06-21 11:30:09 +0100

    initial desktop style

--- Course_02/drone_site/script.js
+++ Course_02/drone_site/script.js
@@ -2,16 +2,10 @@ const ham = document.getElementById("hamburger");
 const closeNav = document.getElementById("close");
 const navBar = document.getElementById("navbar");
 
-
-document.addEventListener("DOMContentLoaded", () => {
-    // your code here
-    ham.addEventListener("click", () => {
-        navBar.style.transform = "translateX(0)";
-    })
-
-    closeNav.addEventListener("click", () => {
-        navBar.style.transform = "translateX(100%)";
-    })
+ham.addEventListener("click", () => {
+    navBar.style.transform = "translateX(0)";
 });
 
-console.log(ham, closeNav, navBar);
+closeNav.addEventListener("click", () => {
+    navBar.style.transform = "translateX(100%)";
+});
