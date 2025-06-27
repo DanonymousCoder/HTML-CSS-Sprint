718069435d1ca4a0ed8a4f9425041d9c690ebc61
Ridwanullah <aladebaruwa@gmail.com>
2025-06-27 09:29:16 +0100

    add.

--- Course_05/index.js
+++ Course_05/index.js
@@ -1,3 +1,11 @@
 /**
  * JS for CSS Variables here
  */
+
+let root = document.querySelector(":root");
+let rootStyles = getComputedStyle(root);
+let black = rootStyles.getPropertyValue("--black");
+
+console.log("red: " + black);
+
+root.style.setProperty("--black", "rgb(56, 6, 6)");
