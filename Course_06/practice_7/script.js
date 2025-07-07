79d57c920ada73c369bbac12c8b2361df6f33d03
Ridwanullah <aladebaruwa@gmail.com>
2025-07-07 13:00:21 +0100

    Navbar up and working 👷

--- Course_06/practice_7/script.js
+++ Course_06/practice_7/script.js
@@ -1,2 +1,11 @@
 let openNav = document.getElementById("open-nav");
 let closeNav = document.getElementById("close-nav");
+let navBar = document.querySelector(".nav-links");
+
+openNav.addEventListener("click", () => {
+    navBar.style.transform = "translateX(0%)";
+})
+
+closeNav.addEventListener("click", () => {
+    navBar.style.transform = "translateY(100%)";
+})
